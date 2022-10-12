398ee2361ef9b06ef9a092b0e04bdbe8095a8580
ajunghans <ajunghans@intershop.com>
2022-10-12 09:32:41 +0200

    RealTime

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import {Task} from '../Task/task.model'
+import { CountService } from '../services/count.service';
+
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+
+  
 })
 export class ListComponent implements OnInit, OnChanges {
 
@@ -14,75 +19,55 @@ export class ListComponent implements OnInit, OnChanges {
   
   singleid:number;
   idback = null;
-  checked = false;
-  sample:number;
+  //countednumber:number = null;
+
+  constructor(private countService: CountService) { }
+
+
 
   getidparent(id:number)
   {
     this.idback = id;
-
     const newCreatedTasks= this.myTasks.filter(eachTask => eachTask.id!==id)
     this.myTasks = newCreatedTasks
     console.log(this.myTasks)
+    
   }
 
-  getchecked(check:boolean)
-  {
-    this.checked = check;
-    this.check(this.checked);
-    console.log(this.checked);
+  updateCounter():void  {
+    
+    const countednumber = this.myTasks.length;
+    this.countService.Counter.next(countednumber);
+    console.log(this.myTasks);
+    console.log(countednumber);
   }
 
   myTasks: Task[] = [
   ];
   
-  check(sample)
-  {
-    if(sample == true)
-    {
-      
-    }
-  }
-
-  // delete()
-  // {
-  
-  //   console.log(this.idback)
-  //   this.myTasks.splice(this.idback-1, 0);
-  //   if(this.myTasks.length[1])
-  //   {
-  //     this.myTasks = [];
-  //   }
-
-  
-  // }
-  constructor() { }
-  
-  button()
-  {
-
-
-  }
-
   ngOnInit(): void {
   
     //To delete first item in Array
-    this.myTasks.splice(0,1,);
+    this.myTasks = [];
   }
 
-  // map, reduce, filter, concat, includes (Array methods)
-  // avoid mutation of arrays
 
-  ngOnChanges():void{
-    console.log(this.todoFromParent);  
+  ngOnChanges():void{  
 
   //Single Components from Model get Stored in array
+  
+    if(!this.todoFromParent){
+      return
+    }
+
     const todoListItem:Task = {dailyTask:this.todoFromParent,checked:false, id: this.myTasks.length +1};
     this.myTasks = this.myTasks.concat([todoListItem]);
     this.singleid = todoListItem.id -1;
+    this.updateCounter();
 
-    console.log(this.myTasks);
 
-    console.log(todoListItem.checked);
+
+
+
 }
 }
